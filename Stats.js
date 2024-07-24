fcb952ee6b77561477bce26cc462d7678a88db28
Amit Kulkarni <154905823+Amit-Webdev@users.noreply.github.com>
2024-07-24 21:31:19 +0530

    Add files via upload

---
+++ Stats.js
@@ -0,0 +1,19 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats"> Start adding something to your packing list 👜 </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! ready to go 🎉✈️"
+          : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
